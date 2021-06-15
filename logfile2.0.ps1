8b79dd41c494c3f99b8e0c8cf91e1b7462c6ac14
gergő szijártó <54061320+geri666@users.noreply.github.com>
2021-06-15 08:32:07 +0200

    first commit

---
+++ logfile2.0.ps1
@@ -0,0 +1,35 @@
+﻿# logfile 2.0
+# created by gergö szijarto 2021
+
+cls
+
+# declare variables such as folders or files
+$path = "C:\temp\NewFolder"
+$pathFile = $path + "\" + $name
+$name = "file.txt"
+
+# check if the file exists
+If(!(test-path $pathFile))
+{
+      echo "logfile created"
+      New-Item -Path $path -ItemType File -Name $name -Force # creates file if doesn't exist
+} else {
+      echo "logfile already exists" # message if it does already exist
+}
+
+# checks if directory already exists
+If(!(test-path $path)){
+      
+      echo "dir created"
+      New-Item -Path $path -ItemType Directory # creates directory if doesn't exist
+} else {
+      echo "dir already exists" # message if it does exist
+}
+
+Add-Content -Path $pathFile -Value (Get-Date) # writes current date and time into the file
+echo "date added"
+
+$userInput = Read-Host "enter a message to add to the file" # reads user input
+
+Add-Content -Path $pathFile -Value $userInput # writes user input into the file
+
